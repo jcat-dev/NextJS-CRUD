78b8adc403e47415ccdf19ee203bd2014227100c
unknown <jec.dev.22@gmail.com>
2023-01-24 16:06:46 -0300

    Add nuevo interface para user

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,6 +1,20 @@
 import { Admin, Student } from "./person"
 import { Role } from "./role"
 
+export interface SearchUser {
+  _id: string
+  role: Role
+  email: string
+}
+
+export interface NewUser {
+  role: Role
+  email: string
+  password: string
+  admin?: string
+  student?: string
+}
+
 export interface User {
   _id: string
   role: Role
