8127d9b7ef0b3a6d86555f9311d10ac107706b1c
unknown <jec.dev.22@gmail.com>
2023-01-24 16:13:31 -0300

    Dashboard

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,26 @@
+import Unauthorized from "@/src/components/Unauthorized"
+import { useSession } from "next-auth/react"
+import NewUser from "./new-user"
+import Student from "./student"
+
+const Dashboard = () => {
+  const { data } = useSession()
+
+  if (data && data.user.role === "admin") {
+    return (
+      <NewUser />
+    )
+  }
+
+  if (data && data.user.role === "student") {
+    return (
+      <Student />
+    )
+  }
+
+  return (
+    <Unauthorized />
+  )
+}
+
+export default Dashboard
---
+++ src/components/Unauthorized.tsx
@@ -0,0 +1,7 @@
+const Unauthorized: React.FC = () => {
+  return (
+    <h1>Unauthorized</h1>
+  )
+}
+
+export default Unauthorized
