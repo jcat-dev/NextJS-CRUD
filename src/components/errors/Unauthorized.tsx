a5a029b2c0d2b5de2208c012ea4042d8c08621d9
unknown <jec.dev.22@gmail.com>
2023-02-03 21:27:23 -0300

    Add: Estilo al Unauthorized component

--- src/components/errors/Unauthorized.tsx
+++ src/components/errors/Unauthorized.tsx
@@ -1,6 +1,20 @@
+import styles from "@/src/styles/module/erros/Unauthorized.module.css"
+
 const Unauthorized: React.FC = () => {
   return (
-    <h1>Unauthorized</h1>
+    <main className={styles.container} >
+      <p className={styles.error} >401</p>
+      <p className={styles.title} >Unauthorized</p>
+      <p className={styles.text} >
+        You need to provide valid credentials to access this resource.
+        <a
+          className={styles.link}
+          href="/"
+        > 
+          Log in
+        </a>
+      </p>
+    </main>
   )
 }
 
