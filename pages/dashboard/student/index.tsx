fb88295c962f1366f85cb3a9120f643b060a9694
unknown <jec.dev.22@gmail.com>
2023-03-03 18:48:43 -0300

    [Student] Add: Control de verificación

--- pages/dashboard/student/index.tsx
+++ pages/dashboard/student/index.tsx
@@ -1,41 +1,9 @@
-import { useSession, signOut } from "next-auth/react"
-import Link from "next/link";
-import { useRouter } from "next/router"
+import VerifyAuthorization from "@/src/components/dashboarVerification/VerifyAuthorization";
 
 export default function Student() {
-  const router = useRouter()
-  const { data: session } = useSession()
-
-  const handleSignOutClick = async () => {
-    const data = await signOut({redirect: false, callbackUrl: '/'})
-    router.push(data.url)
-  }
-
-  if (session && session.user) {
-    return (
-      <>
-        Signed in as {session.user.name} <br />
-        <button onClick={handleSignOutClick}>Sign out</button>
-      </>
-    )
-  }
- 
-
   return (
-    <main  >
-      <h1  >Access denied</h1>
-           
-      <Link href={"/login/sign-in"} >
-        <a  >
-          Sign In
-        </a>
-      </Link>    
-
-      <Link href={"/login"} >
-        <a  >
-          Log In
-        </a>
-      </Link>      
-    </main>
+    <VerifyAuthorization role="student">
+      <main> Hola </main>
+    </VerifyAuthorization>
   )
 }
