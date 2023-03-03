462629747236944d9f8f2c4c784c92659ddb1103
unknown <jec.dev.22@gmail.com>
2023-03-03 19:17:42 -0300

    [Back-API] Remove: Logica en el archivo nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,10 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
-import { getUserWithPopulate } from "@/mongoose/controllers/user.controller"
-import { User } from "@/src/types/user"
+import { findUserWithPopulate } from "@/mongoose/controllers/user.controller"
+import { Credential } from "@/src/types/credential"
 
 const credentials = {
-  email: { label: "email", type: "email" },
+  email: { label: "Email", type: "email" },
   password: { label: "Password", type: "password" },
 }
 
@@ -14,31 +14,15 @@ export const authOptions = {
       id: "credentials",
       name: "Credentials",      
       credentials: credentials,
-      async authorize(credentials) {        
-        const searchUser = {
-          email: credentials?.email,
-          password: credentials?.password
+      async authorize(credentials) { 
+        if (!credentials?.email || !credentials?.password) return null
+
+        const credential: Credential = {
+          email: credentials.email,
+          password: credentials.password
         }
         
-        const data: User = await getUserWithPopulate(searchUser)
-
-        if (data) {
-          const user = {
-            id: data._id,
-            email: data.email,
-            role: data.role
-          }
-
-          if (data.admin) {
-            return { ...user, name: `${data.admin.name} ${data.admin.surname}` }
-          }
-  
-          if (data.student) {
-            return { ...user, name: `${data.student.name} ${data.student.surname}` }
-          }
-        }       
-        
-        return null
+        return await findUserWithPopulate(credential)      
       }
     })
   ],
